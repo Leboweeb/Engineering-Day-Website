3812bac72c4f49b20b74b898c9c1ede24c0f0da8
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2023-02-15 22:23:00 +0200

    make a navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,54 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import Logo from "./assets/bau-logo.png";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <header>
+    <div id="navbar">
+      <a href="#top" id="bau-logo"><img src="{Logo} " alt="Bau Logo" /></a>
+      <div class="nav_text">
+        <a href="#home">Home</a>
+        <a href="#projects">Project</a>
+        <a href="#about">About Us</a>
+        <a href="#contact">Contact Us</a>
+      </div>
+    </div>
+  </header>
 </main>
 
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+<style lang="scss">
+  @mixin stretch_elem() {
+    top: 0;
+    right: 0;
+    left: 0;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  #navbar {
+    display: flex;
+    justify-content: space-between;
+    text-align: center;
+    position: fixed;
+    @include stretch_elem();
+    #bau-logo {
+      padding: 10px;
+      margin-inline: 3rem;
+    }
+    .nav_text {
+      padding: 10px;
+      margin-inline: 3rem;
+      margin-top: 1.5rem;
+      a {
+        margin-inline: 15px;
+        &:hover {
+          color: #18bc9c;
+          background-color: transparent;
+        }
+        text-decoration: none;
+        color: whitesmoke;
+      }
+    }
+
+    background-color: #2c3e50;
+    width: 100%;
+    height: auto;
   }
 </style>
