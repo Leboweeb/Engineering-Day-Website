5195a024d180250930839d0383b461a542a5be67
Leboweeb <74187782+Leboweeb@users.noreply.github.com>
2023-02-15 23:16:46 +0200

    functioning navbar

--- src/App.svelte
+++ src/App.svelte
@@ -15,40 +15,114 @@
     </div>
   </header>
 </main>
+<div class="make_space">
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus dolores eius
+    quibusdam in ad voluptas iste repudiandae, explicabo repellat numquam
+    perspiciatis sit laboriosam voluptatem, excepturi eaque. Deserunt sed error
+    accusantium. Odit ullam debitis itaque fugit, eum officia nobis hic atque.
+    Temporibus vero possimus consequuntur, minima doloremque numquam, placeat
+    esse ipsa beatae assumenda qui consequatur perspiciatis et, consectetur at
+    deserunt sequi! Maxime atque quisquam illum eum doloribus, sunt, cum quae
+    nemo illo veritatis, amet rem recusandae magni error laborum! Expedita,
+    assumenda? Saepe, quibusdam! Maiores voluptatem voluptatum minima,
+    blanditiis ipsum beatae sequi! Dolores deserunt velit nam animi expedita
+    dicta molestias, at vero pariatur quae minus odio modi reiciendis eos est
+    cumque quisquam. Voluptate ratione dolore recusandae consequuntur unde
+    fugiat distinctio harum impedit. Error ea, ipsum totam enim odit corporis
+    numquam deleniti! Nemo odit beatae facere, repudiandae odio autem distinctio
+    culpa sapiente cum exercitationem ipsum molestiae aut dicta sunt enim
+    consectetur minima quam. Consequuntur soluta eligendi molestias fugit
+    officiis facere accusantium illo facilis sed possimus id dolore a neque
+    veniam sequi rem voluptate, perspiciatis magni delectus vitae deserunt minus
+    sit consequatur est. Accusantium! Eveniet ipsam beatae saepe voluptas,
+    voluptatibus earum delectus rem? Blanditiis, recusandae nostrum! Amet, harum
+    labore ullam ducimus optio debitis delectus quasi, vero, quam rerum
+    consequatur officiis mollitia aut? Dolorem, libero! Quos voluptatibus
+    doloremque enim ducimus voluptas, omnis praesentium molestiae nisi atque
+    dolorem quidem laudantium ratione laborum maxime excepturi magnam corporis
+    reprehenderit illo porro? Saepe, doloribus repellat recusandae modi
+    quibusdam laudantium? Nostrum maiores fugit eius. Laboriosam ipsa sint
+    fugiat iure illum reprehenderit eos aut consequatur recusandae molestiae
+    magni ut nesciunt, non vero, amet consectetur voluptas eaque dolorem,
+    temporibus tenetur quod? Ab? Modi voluptatibus inventore, voluptate beatae
+    at reiciendis obcaecati fugit ab alias voluptas omnis ea velit atque.
+    Commodi aspernatur cumque culpa, unde dolorum reprehenderit soluta!
+    Repudiandae omnis eos repellendus pariatur nostrum. Dolore accusamus, enim
+    tempora quas labore aliquam at sequi ullam tenetur alias natus laboriosam
+    voluptas id corrupti eaque quaerat, culpa saepe. Placeat rem soluta
+    voluptatem maxime, dolorem rerum nihil ut! Officia, a fugit. Tempore
+    pariatur perspiciatis voluptas optio a modi similique veritatis placeat,
+    magni dolores laudantium doloribus earum sint adipisci exercitationem
+    necessitatibus est, animi provident, assumenda voluptatibus expedita. In,
+    magnam? Eum minus laudantium necessitatibus placeat illo blanditiis, amet
+    voluptatem? Quo architecto alias nulla error harum pariatur assumenda quod,
+    mollitia beatae aspernatur minima, dignissimos debitis voluptate et odit
+    labore magni sequi. Repudiandae quasi aut iste numquam, dolor natus ducimus?
+    Repudiandae molestiae ad in aliquam minima ipsum doloribus ratione iure
+    neque repellat, voluptate animi cupiditate dolore labore saepe, nam non
+    optio fugiat. Temporibus nisi tempore nam totam aliquid sequi blanditiis!
+    Repellendus earum corrupti commodi aliquam dolorem eius, sunt fugiat ipsam
+    quisquam sapiente voluptatem, voluptatum, pariatur amet exercitationem est
+    ipsum eos. Culpa, corrupti! Pariatur laudantium aut earum quod non facere
+    placeat sapiente dolore omnis impedit accusamus velit quia commodi maxime
+    asperiores perspiciatis laborum, cum est, voluptas nesciunt voluptate et,
+    magnam ducimus! Temporibus, rerum? Dolorum eum quod iusto ratione magnam
+    iure iste commodi debitis? Quod impedit, ullam iusto repellat, rem eveniet
+    eaque laudantium porro quo fugiat architecto, corporis fugit facilis?
+    Suscipit tenetur beatae consequuntur. Libero quasi eos laudantium asperiores
+    omnis totam, odit voluptates, quae eaque accusamus corrupti labore vel unde
+    suscipit id aspernatur. Dolore modi ducimus nemo qui dignissimos fugiat
+    provident, quas eveniet unde. Aut autem, velit aperiam sint nihil possimus
+    consequuntur doloribus sit delectus, ipsam quasi accusantium totam
+    voluptates eaque aliquam, eos dolores. Rem a molestiae libero minus
+    reprehenderit. Commodi perspiciatis similique doloremque? Enim assumenda
+    tempora odit ratione amet sit corporis odio, id quis possimus aliquid modi
+    obcaecati ab velit, iure nobis voluptatibus cumque esse ea accusamus a et?
+    Soluta saepe voluptatibus corrupti. Odit vero placeat aspernatur autem
+    nihil. Provident obcaecati quae qui ducimus iure perspiciatis. Optio, quos
+    dolorum? Doloremque rerum neque labore veniam eligendi repellat
+    exercitationem quidem tenetur sunt minima? Libero, quam. Molestiae, minima
+    debitis. Laudantium fuga possimus vel dolores molestiae, recusandae sapiente
+    nisi reprehenderit accusantium, maiores nulla perspiciatis totam! Quos
+    adipisci rem eligendi voluptatem, deleniti quasi corporis odio magni in
+    commodi. Voluptatum consequuntur consequatur vero. Expedita, nulla! Dolore
+    corrupti quidem veniam velit saepe minima, iure exercitationem aliquid
+    reprehenderit voluptas harum aperiam earum voluptate corporis beatae, sequi
+    amet accusantium, et labore enim. Dolor ad ipsa voluptates doloremque quam
+    excepturi ipsum deleniti laboriosam soluta quidem adipisci animi incidunt
+    voluptatum itaque suscipit, veritatis libero. Numquam nulla rem libero,
+    architecto labore quisquam sapiente dolorem voluptatem? Id alias praesentium
+    sapiente fuga at quibusdam perferendis natus esse suscipit rerum, nisi aut
+    animi porro minima ducimus iusto magnam non nemo earum minus sit molestias
+    quas. Temporibus, tempore eum! Incidunt sapiente eos temporibus voluptatem
+    sed delectus! Eum, magnam hic exercitationem magni, consequatur expedita ad
+    at nemo quibusdam dolorum assumenda quos. Libero molestias quam explicabo
+    consectetur mollitia, inventore ab veritatis. Esse illo rem, officia nam,
+    dignissimos ea, dolorem quis sed natus dicta perspiciatis quia
+    exercitationem assumenda ullam eius consequatur expedita totam blanditiis
+    distinctio. Laudantium voluptatum soluta mollitia, suscipit illum tempora?
+    Dignissimos, doloremque. Error provident ipsam quis corrupti voluptatibus!
+    Necessitatibus rem facilis inventore. Illo quis dolor accusantium quam
+    voluptatem temporibus, totam, quasi cumque molestiae sed ipsa laborum
+    consequuntur ipsum cupiditate dolores. Veniam nulla velit dignissimos
+    deleniti ut distinctio, nihil sit cupiditate. Repudiandae ea, et, illo
+    inventore quaerat vero blanditiis quasi porro libero commodi ducimus ipsa
+    voluptatem pariatur eum fugit, ex corporis! Explicabo possimus quidem hic,
+    nam fugiat quos, rerum earum facere mollitia doloremque nihil natus
+    asperiores sunt consequuntur omnis incidunt culpa ipsum iste! Corporis
+    perferendis in voluptatum deleniti molestias molestiae minima?
+  </p>
+</div>
 
 <style lang="scss">
-  @mixin stretch_elem() {
-    top: 0;
-    right: 0;
-    left: 0;
-  }
-  #navbar {
-    display: flex;
-    justify-content: space-between;
-    text-align: center;
-    position: fixed;
-    @include stretch_elem();
-    #bau-logo {
-      padding: 10px;
-      margin-inline: 3rem;
-    }
-    .nav_text {
-      padding: 10px;
-      margin-inline: 3rem;
-      margin-top: 1.5rem;
-      a {
-        margin-inline: 15px;
-        &:hover {
-          color: #18bc9c;
-          background-color: transparent;
-        }
-        text-decoration: none;
-        color: whitesmoke;
-      }
-    }
-
-    background-color: #2c3e50;
-    width: 100%;
-    height: auto;
-  }
+  //   main {
+  //     background: rgb(84, 76, 122);
+  //     background: linear-gradient(
+  //       142deg,
+  //       rgba(84, 76, 122, 1) 0%,
+  //       rgba(138, 110, 246, 1) 25%,
+  //       rgba(19, 12, 29, 1) 100%
+  //     );
+  //   }
 </style>
